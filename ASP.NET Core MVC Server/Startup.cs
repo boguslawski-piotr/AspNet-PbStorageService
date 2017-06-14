ab58e16a06fbb8dfd8e61d0ef5759610685ee83a
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-14 18:31:40 -0400

    Preparations, tests

--- ASP.NET Core MVC Server/Startup.cs
+++ ASP.NET Core MVC Server/Startup.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace NetCore
+namespace pbXStorage.AspNetCore.MVC
 {
-    public class Startup
+	public class Startup
     {
         public Startup(IHostingEnvironment env)
         {
@@ -24,19 +21,34 @@ namespace NetCore
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+			// Adds a default in-memory implementation of IDistributedCache.
+			services.AddDistributedMemoryCache();
+
+			services.AddSession(options =>
+			{
+				options.CookieSecure = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
+				options.CookieHttpOnly = true;
+				options.CookieName = "pbXStorage";
+
+				// Set a short timeout for easy testing.
+				// TODO: remember about cange to real value after testing
+				options.IdleTimeout = TimeSpan.FromSeconds(10);
+			});
+		}
+
+		// Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+			app.UseSession();
             app.UseMvc();
         }
     }
