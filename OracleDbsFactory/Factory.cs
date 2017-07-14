73347fbaa0ad71045a3c614e78f13c87e0d89171
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-07-13 20:38:31 -0400

    OracleDbsFactory sample

---
+++ OracleDbsFactory/Factory.cs
@@ -0,0 +1,23 @@
+﻿using pbXStorage.Repositories;
+
+namespace Oracle
+{
+	public class OracleDbFactory : IDbFactory
+	{
+		public IDb Create(string connectionString)
+		{
+			// First: install Oracle.ManagedDataAccess NuGet package
+			// Second: uncomment line below
+			//return new DbOnSDC(new Oracle.DataAccess.Client.OracleConnection(connectionString));
+			return null;
+		}
+	}
+
+	public class MySqlFactory : IDbFactory
+	{
+		public IDb Create(string connectionString)
+		{
+			return new DbOnSDC(new MySql.Data.MySqlClient.MySqlConnection(connectionString));
+		}
+	}
+}
